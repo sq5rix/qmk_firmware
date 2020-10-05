a2f5a6b3fc55021dd3d7adad07159bd072a2dccd
Tom <sq5rix@gmail.com>
2020-10-05 14:24:59 +0200

    some more layers

--- keyboards/keebio/iris/keymaps/sq5rix/keymap.c
+++ keyboards/keebio/iris/keymaps/sq5rix/keymap.c
@@ -29,16 +29,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_BEAKL15] = LAYOUT(
      KC_GRV,  KC_4,    KC_0,    KC_1,    KC_2,    KC_3,                               KC_7,    KC_6,    KC_5,    KC_9,    KC_8,    KC_BSLS,    
      KC_TAB,  KC_Q,    KC_H,    KC_O,    KC_U,    KC_X,                               KC_G,    KC_C,    KC_R,    KC_F,    KC_Z,    KC_QUOT,
-     SH_ESC,  KC_Y,    KC_I,    KC_E,    KC_A,    LT_DAT,                              KC_D,    KC_S,    KC_T,    KC_N,    KC_B,    KC_BSPC,   
+     SH_ESC,  KC_Y,    KC_I,    KC_E,    KC_A,    LT_DAT,                             KC_D,    KC_S,    KC_T,    KC_N,    KC_B,    KC_BSPC,   
      KC_LSFT, KC_J,    KC_BSLS, KC_COMM, KC_K,    KC_QUOT, BL_TOGG,          RGB_TOG, KC_W,    KC_M,    KC_L,    KC_P,    KC_V,    KC_RSFT,
                                     KC_LCTL, RAISE, LOWER,                     SH_ESC,   KC_LALT, KC_RALT
   ),
 
   [_LOWER] = LAYOUT(
-     RESET,   KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                            KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PGUP,
-     KC_TILD, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_PGDN,
-     KC_DEL,  _______, KC_LEFT, KC_RGHT, KC_UP,   KC_LBRC,                            RGB_HUD, KC_P4,   KC_P5,   KC_P6,   KC_PLUS, KC_HOME,
-     BL_STEP, BEAKL15, QWERTY,  _______, KC_DOWN, KC_LCBR, KC_LPRN,          KC_RPRN, KC_RCBR, KC_P1,   KC_P2,   KC_P3,   KC_MINS, KC_END,
+     RESET,   KC_F4,   KC_F10,  KC_F1,   KC_F2,   KC_F3,                              KC_F7,   KC_F6,   KC_F5,   KC_F9,   KC_F8,   _______, 
+     _______, _______, _______, _______, _______, _______,                            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_PGDN,
+     _______, KC_BSLS, KC_LPRN, KC_DQT,  KC_RPRN, KC_HASH,                            RGB_HUD, KC_P4,   KC_P5,   KC_P6,   KC_PLUS, KC_HOME,
+     BL_STEP, BEAKL15, QWERTY,  KC_COLN, KC_ASTR, KC_PLUS, _______,                   KC_RPRN, KC_RCBR, KC_P1,   KC_P2,   KC_P3,   KC_MINS, KC_END,
                                     _______, _______, _______,                   _______, _______, _______
   ),
 
