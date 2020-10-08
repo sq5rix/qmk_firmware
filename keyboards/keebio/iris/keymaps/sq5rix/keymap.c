da976a33868b20dac32cd73bc21c024da746de8b
Tom <sq5rix@gmail.com>
2020-10-08 17:35:36 +0200

    bug in windows keys

--- keyboards/keebio/iris/keymaps/sq5rix/keymap.c
+++ keyboards/keebio/iris/keymaps/sq5rix/keymap.c
@@ -87,7 +87,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 void encoder_update_user(uint8_t index, bool clockwise) {
     if (index == 0) {
         if (clockwise) {
-            tap_code16(LCA(KC_LEFT));
+            tap_code16(C(G(KC_LEFT)));
         } else {
             tap_code16(C(G(KC_RGHT)));
         }
