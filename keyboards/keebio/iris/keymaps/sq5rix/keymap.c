0ea186ef67ded229286378846cf4db2b8844eab8
Tom <sq5rix@gmail.com>
2021-01-07 16:00:16 +0100

    tap dance

--- keyboards/keebio/iris/keymaps/sq5rix/keymap.c
+++ keyboards/keebio/iris/keymaps/sq5rix/keymap.c
@@ -11,6 +11,7 @@ enum custom_keycodes {
 
 #define LOWER  LT(_LOWER, KC_ENT)
 #define RAISE  LT(_RAISE, KC_SPC)
+
 #define SH_ESC MT(MOD_LSFT, KC_ESC)
 #define SH_CWD MT(MOD_LALT, KC_SCLN)
 #define SH_LTAB MT(MOD_LALT, KC_TAB)
@@ -18,11 +19,16 @@ enum custom_keycodes {
 #define SH_UND MT(MOD_LCTL, KC_UNDS)
 #define SH_BSPC MT(MOD_LCTL, KC_BSPC)
 
-#define TD_S ACTION_TAP_DANCE_DOUBLE(KC_S, KC_SCLN)
-#define TD_A ACTION_TAP_DANCE_DOUBLE(KC_A, KC_COLN)
-
 #define AL_T ALGR_T(KC_T)
 #define AL_I ALGR_T(KC_I)
+
+// Tap Dance definitions
+enum {
+    TD_S,
+};
+qk_tap_dance_action_t tap_dance_actions[] = {
+    [TD_S] = ACTION_TAP_DANCE_DOUBLE(KC_S, KC_COLN),
+};
 
 bool sh_key(keyrecord_t *record, uint8_t sk, uint8_t nk);
 bool sh_key8(keyrecord_t *record, uint8_t sk, uint8_t nk);
@@ -32,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_BEAKL15] = LAYOUT(
   TG(_RAISE),  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_DEL,
      SH_LTAB,  KC_Q,    KC_H,    KC_O,    KC_U,    KC_X,                               KC_G,    KC_C,    KC_R,    KC_F,    KC_V,    KC_LGUI,
-     SH_UND,   KC_Y,    AL_I,    KC_E,    KC_A,    LT_DAT,                             KC_D,    KC_S,    AL_T,    KC_N,    KC_W,    KC_BSPC,
+     SH_UND,   KC_Y,    AL_I,    KC_E,    KC_A,    LT_DAT,                             KC_D,    TD(TD_S),AL_T,    KC_N,    KC_W,    KC_BSPC,
      KC_LSFT,  KC_J,    KC_SLSH, LT_COM,  KC_K,    LT_QUOT, RGB_TOG,          BL_TOGG, KC_B,    KC_M,    KC_L,    KC_P,    KC_Z,    KC_RSFT,
                                         SH_BSPC, LOWER, RAISE,                      SH_ESC,  SH_RTAB,  SH_CWD
   ),
@@ -133,9 +139,7 @@ const rgblight_segment_t PROGMEM altgr_state[] = RGBLIGHT_LAYER_SEGMENTS(
 // Now define the array of layers. Later layers take precedence
 const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
     lower_layer,
-    raise_layer,
-    shift_state,
-    altgr_state
+    raise_layer
 );
 
 void keyboard_post_init_user(void) {
@@ -149,8 +153,3 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     return state;
 }
 
-bool led_update_user(led_t led_state) {
-    rgblight_set_layer_state(2, led_state.shift);
-    rgblight_set_layer_state(3, led_state.altgr);
-    return true;
-}
